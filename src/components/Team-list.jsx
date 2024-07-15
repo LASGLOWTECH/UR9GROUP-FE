da972fed3b2529a5b1ca9bf5968b33f401580fa2
LASGLOWTECH <omozemojeau@gmail.com>
2024-07-15 11:50:07 +0100

    Changed some files

--- src/components/Team-list.jsx
+++ src/components/Team-list.jsx
@@ -26,23 +26,27 @@ const TeamList = [
 
     {
 
-        Avatar: "Sandra.jpeg",
+        Avatar: "/Hr.jpg",
         Name: "Sandra Udeji",
         icon: "fas fa-star",
         Position: "Human Resource Manager(HR)",
         About: "Miss Sandra Udeji is a highly experienced and accomplished Human Resource professional and Operations Manager at UR9, holding a degree in Bio-Technology from Godfrey Okoye" +
- " University and boasting an impressive track record as a Human Resource Assistant at the National Office for Technology Acquisition and Promotion. With three certifications in Human" +
-"  Resource Management, Human Resource Diploma, and Project Management, Sandra brings a unique blend of strategic insight, operational expertise, and leadership acumen to her role," +
-"skillfully managing all aspects of Human Resources and overseeing the overall operations of the company with precision and finesse, driving efficiency, productivity, and success at UR9."
+            " University and boasting an impressive track record as a Human Resource Assistant at the National Office for Technology Acquisition and Promotion. With three certifications in Human" +
+            "  Resource Management, Human Resource Diploma, and Project Management, Sandra brings a unique blend of strategic insight, operational expertise, and leadership acumen to her role," +
+            "skillfully managing all aspects of Human Resources and overseeing the overall operations of the company with precision and finesse, driving efficiency, productivity, and success at UR9."
 
     },
 
 
 
+
+
+
+
     {
 
-        Avatar: "/Admin5.png",
-        Name: "Abdulmalik-sherif Mustafa",
+        Avatar: "/Abdul.jpg",
+        Name: "Engr. Abdulmalik- Sheriff Mustapha",
         icon: "fas fa-star",
         Position: "Head of Construction",
         About: "Engr. Abdulmalik Mustafa is the Head of Construction at UR9. He is a graduate of Engineering  from the prestigious Ahmadu Bello University, Zaria. After training as an Aircraft Technician  with the Nigerian College of Aviation Technology (NCAT), he joined the real estate industry as  an Engineer with a handful of real estate companies. Also a certified Project Manager and Programmer, he has worked on several successful projects  spanning across property development and web development before assuming his position at UR9  Group.  He is a charismatic and efficient team leader with a keen eye for detail"
@@ -52,46 +56,79 @@ const TeamList = [
 
     {
 
-        Avatar: "/Admin5.png",
+        Avatar: "/Adex.jpg",
         Name: "Adeka Solomon",
         icon: "fas fa-star",
         Position: "Head Brand & Media",
         About: "Adeka Solomon is a dynamic, results-driven executive with years of broad-based, progressive" +
-"experience in Marketing, Sales, Branding, and Product Management. " +
-"An effective combination of creative and analytical leadership skills that drive revenue, and " +
-"increase profit margins and shareholder and Brand value. " +
-"Adept at developing and maintaining strategic partnerships to advance brand awareness, " +
-"product acquisition, and customer loyalty. A results-oriented and well-rounded leader with " +
-"creative approaches to resolving complex issues. Helping brands and businesses speak to the "  +
-"world through big ideas to captivate big audiences."
+            "  experience in Marketing, Sales, Branding, and Product Management. " +
+            "An effective combination of creative and analytical leadership skills that drive revenue, and " +
+            "increase profit margins and shareholder and Brand value. " +
+            "Adept at developing and maintaining strategic partnerships to advance brand awareness, " +
+            "product acquisition, and customer loyalty. A results-oriented and well-rounded leader with " +
+            "creative approaches to resolving complex issues. Helping brands and businesses speak to the " +
+            "world through big ideas to captivate big audiences."
 
 
     },
 
 
 
-    
+
     {
 
-        Avatar: "/Admin5.png",
+        Avatar: "/Agnes.jpeg",
         Name: "Shimaor Dowgo Avadoo",
         icon: "fas fa-star",
         Position: "Site Manager and Estate surveyor",
-        About: "Adeka Solomon is a dynamic, results-driven executive with years of broad-based, progressive" +
-"experience in Marketing, Sales, Branding, and Product Management. " +
-"An effective combination of creative and analytical leadership skills that drive revenue, and " +
-"increase profit margins and shareholder and Brand value. " +
-"Adept at developing and maintaining strategic partnerships to advance brand awareness, " +
-"product acquisition, and customer loyalty. A results-oriented and well-rounded leader with " +
-"creative approaches to resolving complex issues. Helping brands and businesses speak to the "  +
-"world through big ideas to captivate big audiences."
+        About: "Shimaor Dowgo Avadoo, a seasoned real estate professional and licensed estate surveyor with" +
+            " a PGD in Facilities Management and Certification in Project Management, in the construction" +
+            " division at UR9 Group as site manager driving growth and success through her expertise in" +
+            " property valuation, market analysis, project management, and team leadership while overseeing" +
+            " strategic planning, project execution, and site operations, and fostering long-term relationships" +
+            " with clients, partners, and stakeholders."
 
 
     },
 
 
-    
 
+    {
+
+        Avatar: "/Anita.jpg",
+        Name: "Anita Asemokhai Ochuwa",
+        icon: "fas fa-star",
+        Position:"Real Estate Supervisor",
+        About: "Anita Asemokhai Anita Ochuwa, a seasoned Real Estate professional, serves as Real Estate Supervisor at UR9 Group, overseeing property inspections, marketing team" +
+            " management, business growth, and compliance with industry regulations and company standards, consistently exceeding sales targets and revenue goals while enhancing process" +
+            "efficiency and maintaining exceptional professionalism and integrity."
+
+    },
+
+
+
+    {
+
+        Avatar: "/Mercy.jpg",
+        Name: "Mrs. IKueze Mercy Ebere",
+        icon: "fas fa-star",
+        Position: "Accountant",
+        About:"Mrs. Ikueze Mercy Ebere is the Financial Accountant of UR9. A seasoned and certified accounting professional, she is driven by a passion for delivering exceptional financial services." +
+" Leveraging her extensive expertise in financial reporting, taxation, and financial planning, Mercy empowers individuals and organizations to achieve financial prosperity. Her unwavering commitment to excellence has solidified her reputation as a trusted financial advisor and accountant, providing guidance and support that fosters long-term financial success."
+
+    },
+
+
+    {
+
+        Avatar: "/Patience.png",
+        Name: "Aaron Nkasiboi Patience",
+        icon: "fas fa-star",
+        Position: "Fashion Supervisor",
+        About:"Aaron Nkasiobi Patience, Fashion Supervisor: Fashion retail professional with a keen eye for detail and a talent for motivating teams to achieve sales goals, dedicated to delivering exceptional customer service and driving business results, leveraging skills in sales growth, team leadership, visual merchandising, trend analysis, inventory management, customer" +
+"service, and fashion expertise, with a BSc from the prestigious Abia State University and a proven track record of leading high-performing teams and curating stunning store displays to deliver top-tier customer experiences."
+
+    },
 
 
 
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -24,10 +24,10 @@ const Team = () => {
            <Row>
             <Col>
 
-            <div className=' py-5 rounded-3  mt-5'>
+            <div className='d-flex align-items-center flex-column py-5 rounded-3  mt-5'>
             <h1 className=" fw-bolder display-5 text-center text-warning pt-5 " style={{ color: '' }}> Meet the Team</h1>
             
-            <p className=" pt-2 px-2 text-center  pt-3" style={{ fontSize: '20px',color:'white' }}>We are team of experts united by one goal.<br/> To provide excellent and quality services in our areas of operation.</p>
+            <p className="textfield pt-2 text-wrap px-2 text-center  pt-3" style={{ fontSize: '20px',color:'white' }}>We are team of experts united by one goal.<br/> To provide excellent and quality services in our areas of operation.</p>
             </div>
 
             </Col>
@@ -45,12 +45,12 @@ const Team = () => {
                {TeamList.map(Teams => {
                   return (
 
-                     <Col lg={6} md={6} key={Teams.id}>
+                     <Col lg={4} md={6} key={Teams.id}>
 
                         <div className='Team-item   my-2 rounded-4' style={{ boxShadoW: '9px 20px white ', backgroundColor: 'white' }} >
-                           <div className='d-flex flex-row justify-between ' style={{ boxShadoW: '9px 20px white ', backgroundColor: '#aa8305fe' }} >
-                              <div className='img-ito d-flex bg-dark m-2 justify-content-center align-items-center' style={{ width: '', height: '', borderRadius: '100%' }}><img className="icon-im " src={Teams.Avatar} alt="house" style={{  width: '100px', height: '100px', borderRadius: '100%', border:'3px solid white'  }} ></img></div>
-                              <h3 className=" fw-bold fs-6 ps-3 py-4 service-title" style={{ color: 'white' }}>{Teams.Name}</h3>
+                           <div className='d-flex flex-row justify-between align-items-center ' style={{ boxShadoW: '9px 20px white ', backgroundColor: '#aa8305fe' }} >
+                              <div className='img-ito d-flex bg-dark m-2 justify-content-center align-items-center' style={{ width: '', height: '', borderRadius: '100%' }}><img className="icon-im " src={Teams.Avatar} alt="house" style={{  width: '150px', height: '150px', borderRadius: '100%', border:'3px solid white'  }} ></img></div>
+                              <h3 className=" fw-medium fs-5 ps-3 py-4 service-title" style={{ color: 'white' }}>{Teams.Name}</h3>
                            </div>
                            
                            <div className="Rating-icon m-2 py-1 px-2 text-warning ">
