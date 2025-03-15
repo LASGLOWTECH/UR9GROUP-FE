10437ae5d41b72707aa0caa80692c4f59bc926bc
LASGLOWTECH <omozemojeau@gmail.com>
2025-03-15 15:07:40 +0100

    removed  comment

--- src/components/tawkit.jsx
+++ src/components/tawkit.jsx
@@ -1,28 +1,4 @@
-// import React, { useEffect } from 'react';
 
-// const TawkToWidget = () => {
-//   useEffect(() => {
-//     var Tawk_API = Tawk_API || {};
-    
-
-//     (function () {
-//       var s1 = document.createElement("script"),
-//         s0 = document.getElementsByTagName("script")[0];
-//       s1.async = true;
-//       s1.src = 'https://embed.tawk.to/65c0d10e8d261e1b5f5c8434/1hlsj18il';
-//       s1.setAttribute('crossorigin', '*');
-//       s0.parentNode.insertBefore(s1, s0);
-//     })();
-//   }, []);
-
-//   return (
-//     <div>
-//       {/* Additional React components or content can go here */}
-//     </div>
-//   );
-// };
-
-// export default TawkToWidget;
 import { useEffect } from "react";
 
 const TawkToWidget = () => {
