c29293ea8fe2a0cfc78a73bf9ea4ea0cd0c3fd13
Isaac-WayneX <isaacwayneagabi@gmail.com>
2025-08-18 15:53:48 +0100

    updated blog external links

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,34 +1,35 @@
-
 import React from "react";
 
 const Blogs = () => {
-     return (
-          <div style={{ 
-               position: 'fixed', 
-               top: '-1px', 
-               left: 0, 
-               width: '100vw', 
-               height: '100vh', 
-               zIndex: 9999,
-               margin: 0,
-               padding: 0,
-               overflow: 'hidden'
-          }}>
-               <iframe
-                    src="https://ur9blog.vercel.app/"
-                    title="UR9 Blog"
-                    style={{
-                         width: '100%',
-                         height: '100%',
-                         border: 'none',
-                         margin: 0,
-                         padding: 0
-                    }}
-                    allowFullScreen
-                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation"
-               />
-          </div>
-     );
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "-1px",
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 9999,
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src="https://ur9blog.vercel.app/"
+        title="UR9 Blog"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          margin: 0,
+          padding: 0,
+        }}
+        allowFullScreen
+        sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
+      />
+    </div>
+  );
 };
 
 export default Blogs;
