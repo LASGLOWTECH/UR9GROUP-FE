21238685cccc48d30d7c115deaa3a6d9ec8456ff
IsaacWayneX <isaacwayneagabi@gmail.com>
2026-02-25 08:57:43 +0100

    feat: Add a new "Partner" entry with its corresponding image to the partners list.

--- src/components/partners3.jsx
+++ src/components/partners3.jsx
@@ -1,44 +1,50 @@
 
 
 const ourPartners = [
-   
-   {
-       id: 1,
-     Banklog: "/Access.png",
-       BankName: "Access Bank" ,
-      
-},
 
-// {
-//    id: 2,
-//  Banklog: "/Chartered.png",
-//    BankName: "Standard Chartered Bank",
-  
-// },
-{
-   id: 3,
- Banklog: "/Fidelity.png",
-   BankName: "Fidelity Bank",
-  
-},
-{
-   id: 3,
- Banklog: "/ZenithBank.png",
-   BankName: "Zenith Bank",
-  
-},
-{
-  id: 4,
-Banklog: "/axa-logo.png",
-  BankName: "Zenith Bank",
- 
-},
-{
-  id: 5,
-Banklog: "/new/elevator.jpg",
-  BankName: "EOPRIEX ELEVATOR",
- 
-},
+  {
+    id: 1,
+    Banklog: "/Access.png",
+    BankName: "Access Bank",
+
+  },
+
+  // {
+  //    id: 2,
+  //  Banklog: "/Chartered.png",
+  //    BankName: "Standard Chartered Bank",
+
+  // },
+  {
+    id: 3,
+    Banklog: "/Fidelity.png",
+    BankName: "Fidelity Bank",
+
+  },
+  {
+    id: 3,
+    Banklog: "/ZenithBank.png",
+    BankName: "Zenith Bank",
+
+  },
+  {
+    id: 4,
+    Banklog: "/axa-logo.png",
+    BankName: "Zenith Bank",
+
+  },
+  {
+    id: 5,
+    Banklog: "/new/elevator.jpg",
+    BankName: "EOPRIEX ELEVATOR",
+
+  },
+  {
+    id: 6,
+    Banklog: "/partner.jpeg",
+    BankName: "Partner",
+
+  },
 
 
 ]
